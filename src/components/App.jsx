2a6fb80493cc0cc44170b0f9fc8f125431d0a6a5
Gagan Preet <gaganpreet.dev@gmail.com>
2021-05-11 12:42:22 +0530

    update FName and LName on typing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+
+  function handleChangeFname(event) {
+    console.log(event.target.value);
+    setFName(event.target.value);
+  }
+  function handleChangeLname(event) {
+    console.log(event.target.value);
+    setLName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fName} {lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={handleChangeFname}
+          name="fName"
+          placeholder="First Name"
+          value={fName}
+        />
+        <input
+          onChange={handleChangeLname}
+          name="lName"
+          placeholder="Last Name"
+          value={lName}
+        />
         <button>Submit</button>
       </form>
     </div>
